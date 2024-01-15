e5b521cad30159d176d56f6c8913ef5f982c6dcd
shanbora <chanbora@logilink.fr>
2024-01-15 17:15:22 +0100

    page fokus

---
+++ src/Controller/App/Produits/ApplicationsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\App\Produits;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/creation-applications-mobile-tablette', name: 'app_applications_')]
+class ApplicationsController extends AbstractController
+{
+    #[Route('/application-etat-des-lieux-fokus', name: 'fokus')]
+    public function fokus(): Response
+    {
+        return $this->render('app/pages/applications/products/fokus.html.twig');
+    }
+}
