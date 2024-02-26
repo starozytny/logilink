ef6d43f042b85b931512d2d85214d9796e17a15f
shanbora <chanbora@logilink.fr>
2024-02-26 11:27:57 +0100

    page invoices

--- webpack.config.js
+++ webpack.config.js
@@ -93,6 +93,7 @@ Encore
     .addEntry('user_app', './assets/user/js/app.js')
     .addEntry('user_homepage', './assets/user/js/pages/homepage.js')
     .addEntry('user_profil', './assets/user/js/pages/profil.js')
+    .addEntry('user_invoices', './assets/user/js/pages/invoices.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
